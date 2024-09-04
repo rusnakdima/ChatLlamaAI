9ac19d59674d922bf237ed81d3b1658afe28c5fd
Dmitriy Rusnak <rusnakdima03@gmail.com>
2024-09-04 20:25:07 +0600

    Deleted http option

--- src/app/helpers/http-options.ts
+++
@@ -1,9 +0,0 @@
-import { HttpHeaders } from "@angular/common/http";
-
-export const httpOptions = {
-  headers: new HttpHeaders({
-    'Authorization': `Bearer ${(localStorage['token'] ?? sessionStorage['token']) ?? ''}`,
-    'Content-Type': 'application/json',
-    'X-Client-Hostname': window.location.host,
-  })
-};
