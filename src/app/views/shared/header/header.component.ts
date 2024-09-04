042a517b574036b5e05914882c21d8251f23d285
Dmitriy Rusnak <rusnakdima03@gmail.com>
2024-09-04 18:37:39 +0600

    Added user menu

--- src/app/views/shared/header/header.component.ts
+++ src/app/views/shared/header/header.component.ts
@@ -1,25 +1,41 @@
 /* system libraries */
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+/* materials */
+import { MatMenuModule } from '@angular/material/menu';
+
+/* services */
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatMenuModule],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  constructor() {}
+  constructor(
+    private authService: AuthService
+  ) {}
 
   @Output() isShowNavEvent: EventEmitter<boolean> = new EventEmitter();
 
   themeVal: string = '';
 
+  username: string = '';
+  role: string = '';
+  image: string = '';
+
   isShowNav: boolean = false;
 
   ngOnInit(): void {
     this.themeVal = localStorage.getItem('theme') ?? '';
+    this.username = this.authService.getValueByKey("username") ?? '';
+    this.role = this.authService.getValueByKey("role") ?? 'user';
+    this.image = this.authService.getValueByKey("image") ?? '/assets/images/user.png';
   }
 
   showNav() {
@@ -31,5 +47,9 @@ export class HeaderComponent {
     document.querySelector('html')!.setAttribute("class", theme);
     localStorage.setItem('theme', theme);
     this.themeVal = theme;
+  }
+
+  logout() {
+    this.authService.logout();
   }
 }
