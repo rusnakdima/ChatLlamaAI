8299e997f3dce50a4b48fcd7bab62279547ae1e0
Dmitriy Rusnak <rusnakdima03@gmail.com>
2024-09-06 16:46:14 +0600

    Added path to share links page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { ChangePasswordComponent } from "@views/change-password/change-password.
 import { ResetPasswordComponent } from "@views/reset-password/reset-password.component";
 
 import { ChatComponent } from "@views/chat/chat.component";
+import { SharedLinksComponent } from "@views/shared-links/shared-links.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, title: 'Home', canActivate: [canActivateAuth] },
@@ -25,6 +26,6 @@ export const routes: Routes = [
   { path: 'change_password', component: ChangePasswordComponent, title: 'Change Password', data: { breadcrumb: 'Change Password' } },
 
   { path: 'chat/:id', component: ChatComponent, title: 'Chat', canActivate: [canActivateAuth] },
-  { path: 'shared_links', component: HomeComponent, title: 'Shared Links', canActivate: [canActivateAuth] },
+  { path: 'shared_links', component: SharedLinksComponent, title: 'Shared Links', canActivate: [canActivateAuth] },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
