27061b917720a94f413e9c595c3b8218e3cadee0
Dmitriy Rusnak <rusnakdima03@gmail.com>
2024-09-12 14:19:17 +0600

    Changed url for connect to local db

--- src-tauri/src/services/mongodb.rs
+++ src-tauri/src/services/mongodb.rs
@@ -1,7 +1,7 @@
 use mongodb::{error::Error, Client, Database};
 
 pub async fn connect_db() -> Result<Database, Error> {
-  let mongodb_uri: &str = "mongodb://192.168.1.7:27017/ChatLlamaAI";
+  let mongodb_uri: &str = "mongodb://127.0.0.1:27017/ChatLlamaAI";
   // let mongodb_uri: &str = "mongodb+srv://Dmitriy303:dmitriy303369@cluster0.nnrtw4y.mongodb.net/ChatLlamaAI?retryWrites=true&w=majority&appName=Cluster0";
 
   let client = Client::with_uri_str(mongodb_uri).await?;
