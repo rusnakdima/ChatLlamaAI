84ab77df1d52c65e3139ea88d4a18d696ad2849c
Dmitriy Rusnak <rusnakdima03@gmail.com>
2024-09-13 16:41:54 +0600

    Fixed set data to token

--- src-tauri/src/services/manage_token.rs
+++ src-tauri/src/services/manage_token.rs
@@ -7,9 +7,8 @@ use crate::models::user_data::UserData;
 pub fn create(user_data: UserData) -> Result<String, Error> {
   let secret: String = "q3tb@!jnk$^hg8*u5zv9&m6x1s2w7p4r0e3".to_string();
 
-  let temp_data: String = serde_json::to_string(&user_data).unwrap();
   let c = json!({
-    "data": temp_data,
+    "data": &user_data,
     "exp": chrono::Utc::now().checked_add_days(Days::new(7)).unwrap().timestamp()
   });
 
